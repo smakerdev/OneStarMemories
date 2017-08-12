c4c2910be95254d43ab9f1ce412277b74f768314
PpalPpaLi <kdgbang@gmail.com>
2017-08-13 02:06:57 +0900

    make main activity

--- app/OneStarMemories/app/src/main/java/com/example/android/onestarmemories/MainActivity.java
+++ app/OneStarMemories/app/src/main/java/com/example/android/onestarmemories/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.onestarmemories;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tsengvn.typekit.TypekitContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+
     }
 }
